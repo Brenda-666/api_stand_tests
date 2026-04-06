69070424f93f2618b11be65f3c276bb53be057b6
Brenda Garcia <bg171122@gmail.com>
2026-04-06 09:20:27 +0200

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-12ee0e7e-0851-4d9d-b8ab-312e2c2bf9a3.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH ="/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
